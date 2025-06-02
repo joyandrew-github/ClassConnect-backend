a7a94046fb71bb620f7f6a0f88fb340bd4c2b6f4
JoyAndrew S <153337318+joyandrew-github@users.noreply.github.com>
2025-06-02 14:41:43 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://127.0.0.1:27017/elearning', {
+    const conn = await mongoose.connect('mongodb+srv://joyandrew006:andr2006@cluster0.ovsoj.mongodb.net/elearning', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
